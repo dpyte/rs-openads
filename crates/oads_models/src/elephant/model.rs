42d4e83197de12efb4dd8f03a45f051aa781f844
Dhruv Patel <pateldrv456@gmail.com>
2022-06-15 23:48:33 -0400

    TODO: Fix LibTorch Runtime error caused by Elephant's pretrained model

--- crates/oads_models/src/elephant/model.rs
+++ crates/oads_models/src/elephant/model.rs
@@ -1,43 +1,69 @@
-use log::warn;
-use tch::nn::VarStore;
-use std::collections::HashMap;
 use std::fs::File;
-use std::hash::BuildHasherDefault;
+use tch::nn::VarStore;
+use log::{debug, warn};
+use std::collections::HashMap;
 use std::io::{BufReader, Lines};
 
-
+// Path to pretrained model
+static PRETRAINED_MODEL: &str = "/var/system/openads/config/models/elephant/scad_elephantnn_model.pt";
 // Lets the system know whether to execute the model on CPU or GPU
 static MODEL_EXECUTION_TYPE: &str = "/var/system/openads/config/models/elephant/elephant.config";
 
+fn process_config_file(lines: Lines<BufReader<File>>, h_map: &mut HashMap<String, String>) {
+	let mut content: Vec<String> = vec![];
+	for line in lines {
+		if let Ok(line) = line {
+			let first_split: Vec<&str> = line.split("=").collect();
+			match first_split[0].to_string().as_str() {
+				"executor" => {
+					let key = String::from(first_split[0].to_string());
+					let value = String::from(first_split[1].to_string());
+					debug!("Inserting {:?} -> {:?}", key, value);
+					h_map.insert(key, value);
+				},
+				_ => { }
+			}
+		}
+	}
+}
+
 fn get_model_params() -> HashMap<String, String> {
 	let mut retval = HashMap::new();
-
-	let mut data: Lines<BufReader<File>>;
-	let mut use_config_data = true;
+	let data: Lines<BufReader<File>>;
 	match crate::read_lines(MODEL_EXECUTION_TYPE) {
-		Ok(buffer) => { data = buffer },
-		Err(_) => { use_config_data = false }
+		Ok(buffer) => {
+			process_config_file(buffer, &mut retval);
+		},
+		Err(_) => {
+			warn!("Unable to open {}. Use default params ...", MODEL_EXECUTION_TYPE);
+			retval.insert(String::from("executor"), String::from("CPU"));
+		}
 	};
-
-	if !use_config_data {
-		warn!("Unable to open {}. Use default params ...", MODEL_EXECUTION_TYPE);
-		retval.insert(String::from("executor"), String::from("cpu"));
-	} else {
-
-	}
-
 	retval
 }
 
-
+/// Core component of this file - contains pretrained model and corresponding data
 pub struct Elephant {
 	varstore: VarStore,
 }
 
 impl Elephant {
 	pub fn new() -> Self {
-		let _ = get_model_params();
-		let varstore = tch::nn::VarStore::new(tch::Device::Cpu);
+		let params = get_model_params();
+
+		let device_type = match params.get("executor").unwrap().to_string().as_str() {
+			// Current model is restricted to rely on CPU to execute current model
+			_ => tch::Device::Cpu
+		};
+		warn!("Setting device type for elephant to {:?}", device_type);
+		let varstore = VarStore::new(device_type);
+
+		// !FIXME
+		// Current ptm is causing Runtime error possibly due to the ptm being trained on CUDA instead of using CPU.
+		// openads-models will have a cpu-based model available in its next pr.
+		let model = tch::CModule::load(PRETRAINED_MODEL).expect("FAILURE loading pretrained model for Elephant");
+		debug!("Successfully loaded model for elephant with @params {:?}", model);
+
 		Self { varstore }
 	}
 }
