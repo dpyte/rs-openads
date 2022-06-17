485ef7df981f2609fe8d3e48b85d3685859ff7b3
Dhruv Patel <pateldrv456@gmail.com>
2022-06-17 17:23:11 -0400

    remove camera module and merged it into vision

--- crates/oads_camera/src/lib.rs
+++ crates/oads_camera/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate core;
 
-use std::fs::File;
 use std::io;
+use std::fs::File;
 use std::io::BufRead;
 use std::path::Path;
 
@@ -9,7 +9,6 @@ pub mod info;
 pub mod scan;
 pub mod read;
 pub mod vision;
-mod camera;
 mod storage;
 
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
