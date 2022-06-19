3d396551c788a7d06139c158bbce976493d3c17f
Dhruv Patel <pateldrv456@gmail.com>
2022-06-18 23:55:06 -0400

    create mode for execution

---
+++ crates/oads_camera/src/process/processing.rs
@@ -0,0 +1,11 @@
+use opencv::{imgproc, types};
+use opencv::core::Mat;
+
+/// Detect any facial presence in the frame using the opencv backend
+#[inline]
+pub fn detect_facial_presence(&frame: Mat) {
+	let mut gray = Mat::default();
+	let _ = imgproc::cvt_color(&frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0);
+
+	let mut faces = types::VectorOfRect2d::new(); 
+}
