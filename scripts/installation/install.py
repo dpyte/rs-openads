c47e1adaf1294d508439ab22bbf132f5f4dceb09
Dhruv Patel <pateldrv456@gmail.com>
2022-06-26 02:00:56 -0400

    reformat

--- scripts/installation/install.py
+++ scripts/installation/install.py
@@ -1,8 +1,6 @@
 #!bin/python3
-
 import os
 import sys
-import shutil
 
 from pathlib import Path
 from shutil import copyfile
@@ -32,84 +30,77 @@ root_path = '/var/system/openads'
 copy_root_path = os.path.abspath('../../configuration')
 
 system_level_dirs = ['config', 'execution', 'storage']
-config_sub_dirs   = ['camera', 'log', 'models', 'models', 'storage']
+config_sub_dirs = ['camera', 'log', 'models', 'models', 'storage']
 sublevel_directories = {
-    'config': config_sub_dirs,
-    'execution': [],
-    'storage': [],
+	'config': config_sub_dirs,
+	'execution': [],
+	'storage': [],
 }
 
-def generate_directories():
-    print('- Creating directories')
-    # Create root dir
-    path = Path(root_path)
-    path.parent.mkdir(parents=True, exist_ok=True)
-    
-    # Setup first level directories
-    for sl in system_level_dirs:
-        to_generate = os.path.join(root_path, sl)
-        print('├── Setting up {}'.format(to_generate))
-        # sub_root = Path(to_generate)
-        # sub_root.parent.mkdir(parents=True, exist_ok=True)
-        os.makedirs(to_generate, exist_ok=True) 
-        
-        # Setup second level directories
-        contains_sub = sublevel_directories[sl]
-        if contains_sub:
-            for x in contains_sub:
-                level_dir = os.path.join(to_generate, x)
-                print('    ├── {}'.format(level_dir))
-                os.makedirs(level_dir, exist_ok=True)
-                # path = Path(level_dir)
-                # path.parent.mkdir(parents=True, exist_ok=True)
 
-                if x == 'camera':
-                    t_level_dir = os.path.join(level_dir, 'info')
-                    os.makedirs(t_level_dir, exist_ok=True)
-                    # path = Path(t_level_dir)
-                    # path.parent.mkdir(parents=True, exist_ok=True)
-                    print('        ├── {}'.format(t_level_dir))
-                elif x == 'models':
-                    for d in ['elephant', 'facial']:
-                        t_level_dir = os.path.join(level_dir, d)
-                        os.makedirs(t_level_dir, exist_ok=True)
-                        # path = Path(t_level_dir)
-                        # path.parent.mkdir(parents=True, exist_ok=True)
-                        print('        ├── {}'.format(t_level_dir))
-                        
+def generate_directories():
+	print('- Creating directories')
+	# Create root dir
+	path = Path(root_path)
+	path.parent.mkdir(parents=True, exist_ok=True)
+
+	# Setup first level directories
+	for sl in system_level_dirs:
+		to_generate = os.path.join(root_path, sl)
+		print('├── Setting up {}'.format(to_generate))
+		os.makedirs(to_generate, exist_ok=True)
+
+		# Setup second level directories
+		contains_sub = sublevel_directories[sl]
+		if contains_sub:
+			for x in contains_sub:
+				level_dir = os.path.join(to_generate, x)
+				print('    ├── {}'.format(level_dir))
+				os.makedirs(level_dir, exist_ok=True)
+
+				if x == 'camera':
+					t_level_dir = os.path.join(level_dir, 'info')
+					os.makedirs(t_level_dir, exist_ok=True)
+					print('        ├── {}'.format(t_level_dir))
+				elif x == 'models':
+					for d in ['elephant', 'facial']:
+						t_level_dir = os.path.join(level_dir, d)
+						os.makedirs(t_level_dir, exist_ok=True)
+						print('        ├── {}'.format(t_level_dir))
+
 
 def copy_files():
-    mapped_files = [
-        # Camera info
-        (os.path.join(copy_root_path, 'camera', 'info.xml'),        os.path.join(root_path, 'config', 'camera', 'info')),
-        # Log
-        (os.path.join(copy_root_path, 'log', 'log_config.yaml'),    os.path.join(root_path, 'config', 'log')),
-        # Execution,
-        (os.path.join(copy_root_path, 'execution', 'exec'),         os.path.join(root_path, 'execution')),
-        # Storage 
-        (os.path.join(copy_root_path, 'storage', 'capacity'),       os.path.join(root_path, 'config', 'storage')),
-        # Elephant
-        (os.path.join(copy_root_path, 'models', 'elephant.config'), os.path.join(root_path, 'config', 'models', 'elephant')),
-        (os.path.join(copy_root_path, 'models', 'scad_elephantnn_model.pt'), os.path.join(root_path, 'config', 'models', 'elephant')),
-    ]
-    print('Copying files ...')
-    for _, (copy_from, copy_to) in enumerate(mapped_files):
-        filename = os.path.basename(copy_from)
-        
-        to_copy = os.path.join(copy_to, filename)
-        print('- copying {} -> {}'.format(copy_from, to_copy))
-        copyfile(copy_from, to_copy)
+	mapped_files = [
+		# Camera info
+		(os.path.join(copy_root_path, 'camera', 'info.xml'), os.path.join(root_path, 'config', 'camera', 'info')),
+		# Log
+		(os.path.join(copy_root_path, 'log', 'log_config.yaml'), os.path.join(root_path, 'config', 'log')),
+		# Execution,
+		(os.path.join(copy_root_path, 'execution', 'exec'), os.path.join(root_path, 'execution')),
+		# Storage
+		(os.path.join(copy_root_path, 'storage', 'capacity'), os.path.join(root_path, 'config', 'storage')),
+		# Elephant
+		(os.path.join(copy_root_path, 'models', 'elephant.config'),
+		 os.path.join(root_path, 'config', 'models', 'elephant')),
+		(os.path.join(copy_root_path, 'models', 'scad_elephantnn_model.pt'),
+		 os.path.join(root_path, 'config', 'models', 'elephant')),
+	]
+	print('Copying files ...')
+	for _, (copy_from, copy_to) in enumerate(mapped_files):
+		filename = os.path.basename(copy_from)
+
+		to_copy = os.path.join(copy_to, filename)
+		print('- copying {} -> {}'.format(copy_from, to_copy))
+		copyfile(copy_from, to_copy)
 
 
 def main():
-    generate_directories()
-    copy_files()
-    
+	generate_directories()
+	copy_files()
+
 
 if __name__ == '__main__':
-    is_root = os.geteuid() == 0
-    if is_root:
-        sys.exit('Do NOT execute this script as root')
-    main()
-    
-
+	is_root = os.geteuid() == 0
+	if is_root:
+		sys.exit('Do NOT execute this script as root')
+	main()
